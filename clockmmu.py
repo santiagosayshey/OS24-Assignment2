75453a85cb5fe504b2c44f077b08a70702d9efa1
santiagosayshey <chaucted@gmail.com>
2024-08-30 05:29:34 +0930

    revert

--- clockmmu.py
+++ clockmmu.py
@@ -45,7 +45,6 @@ class ClockMMU(MMU):
             self._replace_page(page_number)
 
     def _replace_page(self, page_number):
-        start_hand = self.clock_hand
         while True:
             current_page = list(self.page_table.keys())[self.clock_hand]
             if not self.page_table[current_page][0]:
@@ -59,9 +58,7 @@ class ClockMMU(MMU):
                     print(f"Page {page_number} loaded into memory (replacing page {current_page})")
                 break
             else:
-                # Only reset the reference bit if we've made a full cycle
-                if self.clock_hand == start_hand:
-                    self.page_table[current_page][0] = False  # Reset reference bit
+                self.page_table[current_page][0] = False  # Reset reference bit
             self.clock_hand = (self.clock_hand + 1) % self.frames
 
     def get_total_disk_reads(self):
